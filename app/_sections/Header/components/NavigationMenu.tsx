237d9e0de111a3fe603dd0e7a310a09ab53be050
Ilia Orlov <novailoveyoux@gmail.com>
2024-07-30 02:48:49 +0300

    refactor

--- app/_sections/Header/components/NavigationMenu.tsx
+++ app/_sections/Header/components/NavigationMenu.tsx
@@ -16,12 +16,6 @@ export default function NavigationMenu() {
   return (
     <BaseNavigationMenu>
       <NavigationMenuList className='flex gap-4'>
-        {/* <NavigationMenuItem>
-          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>Link</NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem> */}
         <NavigationMenuItem>
           <ThemeToggle />
         </NavigationMenuItem>
